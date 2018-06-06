8b735bd889106c8f3a9957402c1a4c78ed28e574
xuxy <xuxy@fengyuntec.com>
2018-06-06 18:19:44 +0800

    add k8s

---
+++ src/main/java/uaa/service/K8sService.java
@@ -0,0 +1,53 @@
+package uaa.service;
+
+
+import com.google.gson.reflect.TypeToken;
+import io.kubernetes.client.ApiClient;
+import io.kubernetes.client.ApiException;
+import io.kubernetes.client.Configuration;
+import io.kubernetes.client.apis.CoreV1Api;
+import io.kubernetes.client.models.V1Namespace;
+import io.kubernetes.client.models.V1Pod;
+import io.kubernetes.client.models.V1PodList;
+import io.kubernetes.client.util.Config;
+import io.kubernetes.client.util.Watch;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.IOException;
+
+@Transactional
+@Service
+/**
+ * k8s的demoAPI测试
+ */
+public class K8sService {
+    //list all pods:
+    private  void demo1(String[] args) throws IOException, ApiException {
+        ApiClient client = Config.defaultClient();
+        Configuration.setDefaultApiClient(client);
+
+        CoreV1Api api = new CoreV1Api();
+        V1PodList list = api.listPodForAllNamespaces(null, null, null, null, null, null, null, null, null);
+        for (V1Pod item : list.getItems()) {
+            System.out.println(item.getMetadata().getName());
+        }
+    }
+
+    //watch on namespace object:
+    private  void demo2(String[] args) throws IOException, ApiException{
+        ApiClient client = Config.defaultClient();
+        Configuration.setDefaultApiClient(client);
+
+        CoreV1Api api = new CoreV1Api();
+
+        Watch<V1Namespace> watch = Watch.createWatch(
+            client,
+            api.listNamespaceCall(null, null, null, null, null, 5, null, null, Boolean.TRUE, null, null),
+            new TypeToken<Watch.Response<V1Namespace>>(){}.getType());
+
+        for (Watch.Response<V1Namespace> item : watch) {
+            System.out.printf("%s : %s%n", item.type, item.object.getMetadata().getName());
+        }
+    }
+}
