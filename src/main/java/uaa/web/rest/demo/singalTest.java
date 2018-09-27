ba8aee90e57295ac4dbf8a28114ea86aa2f0a456
xuxy <xuxy@fengyuntec.com>
2018-09-27 16:08:30 +0800

    test signal

---
+++ src/main/java/uaa/web/rest/demo/singalTest.java
@@ -0,0 +1,20 @@
+package uaa.web.rest.demo;
+
+import io.netty.util.Signal;
+import sun.misc.SignalHandler;
+
+public class singalTest implements SignalHandler{
+    public  static void  main(String[] args){
+        Signal sig = new Signal(getOSSignalType());
+//        Signal.handle(sig, this.handle(sig););
+    }
+    private static  String getOSSignalType(){
+        return System.getProperties().getProperty("os.name").toLowerCase()
+            .startsWith("win")?"INI":"USR2";
+    }
+
+    @Override
+    public void handle(sun.misc.Signal signal) {
+
+    }
+}
